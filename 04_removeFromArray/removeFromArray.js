b84772959e3ac7085bdffabac0d700598cf52bbf
Avery <averygadd7@gmail.com>
2023-02-15 14:07:46 -0700

    remove exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    return array.filter(x => !theArgs.includes(x));
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

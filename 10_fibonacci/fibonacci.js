e6cad386e86ff67b902e27afd1bba3e0b4102f2a
Avery <averygadd7@gmail.com>
2023-03-02 16:16:52 -0700

    finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+if (num < 0) return 'OOPS';
+if (num === 0) return 0;
+let n1 = 0, n2 = 1;
+for (let i = 1; i < num; i++){
+    const sum = n2;
+    n2 = n1 + n2;
+    n1 = sum
+    }
+    return n2;
 };
 
 // Do not edit below this line

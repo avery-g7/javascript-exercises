8f62cb9dc44facf37badfa0e69f69e63c13110e3
Avery <averygadd7@gmail.com>
2023-03-02 16:17:18 -0700

    finished age finder exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(age) {
+    return age.reduce((total, person) => {
+        const oldest = getAge(total.yearOfBirth, total.yearOfDeath);
+        const currentPerson = getAge(person.yearOfBirth, person.yearOfDeath);
+        return oldest < currentPerson ? person : total;
+    });
 };
+
+function getAge(birth, dead) {
+    if (!dead) {
+        dead = new Date().getFullYear();
+    }
+    return dead - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;

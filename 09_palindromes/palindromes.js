00a2a206fb041b78b37a5f6cd7dfe8dfe7cd0212
Avery <averygadd7@gmail.com>
2023-03-02 16:16:39 -0700

    finished palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+const reverseStr = str.toLowerCase().replace(/[^A-Za-z0–9]/g, '');
+return reverseStr.split('').reverse().join('') == reverseStr;
 };
 
 // Do not edit below this line

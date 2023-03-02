06ca26326c68001b978e0dfed4eadaa319893068
Avery <averygadd7@gmail.com>
2023-03-02 16:16:28 -0700

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(first, next) {
+  return first + next;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, next) {
+	return first - next;
 };
 
-const sum = function() {
-	
+const sum = function(number) {
+  return number.reduce((total, currentTotal) => total + currentTotal, 0);
 };
 
-const multiply = function() {
+const multiply = function(number) {
+  return number.reduce((total, currentTotal) => total * currentTotal);
 
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number, factor) {
+	if (number === 0 || number === 1) {
+    return 1;
+  }
+  for (let i = number - 1; i >= 1; i--) {
+    number *= i;
+  }
+  return number
 };
 
 // Do not edit below this line
